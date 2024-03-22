c0deed2028cfb68f3c545a52cfd7455678d943ae
Hostinger <mawulikofiagbenyo@gmail.com>
2024-03-22 01:44:25 +0000

    Committing local changes to DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(1)->create();
+        User::factory(1)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
